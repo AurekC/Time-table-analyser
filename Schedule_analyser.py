cd9cb4095f4670cbf3f37bd36c0bdc27934f0cd1
Aurek <aurekchattopadhyay@gmail.com>
2020-08-01 13:00:30 +0530

    Took input of dates from user

--- Schedule_analyser.py
+++ Schedule_analyser.py
@@ -1,8 +1,22 @@
 import pandas as pd
 
+import datetime
+
 df=pd.read_excel(r'Schedule.xlsx')
-print(df)
-print("The first row is \n")
-print (df.values[0])
+#print(df)
+print("The first 10 rows are \n")
+for i in range(10):
+   print (df.values[i],"\n")
 print("The dates are")
 print(df['Date'])
+print("Please enter Starting date \n")
+year = int(input('Enter a year'))
+month = int(input('Enter a month'))
+day = int(input('Enter a day'))
+date1 = datetime.date(year, month, day)
+
+print("Please enter Ending date \n")
+year = int(input('Enter a year'))
+month = int(input('Enter a month'))
+day = int(input('Enter a day'))
+date2 = datetime.date(year, month, day)
