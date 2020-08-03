58a1a6abb83c5e9382b79a56052f6524e91c7ba7
aurek <aurekchattopadhyay@gmail.com>
2020-08-03 13:10:20 +0530

    Successfully calculated total duration of studies in a given time period.
    Unable to cacluate average as unable to caculate total no. of days in given duration because of Nat problem.

--- Schedule_analyser.py
+++ Schedule_analyser.py
@@ -1,50 +1,46 @@
 import pandas as pd
+import numpy as np
 
 import datetime
+import math
 
 df=pd.read_excel(r'Schedule.xlsx')
-#print(df)
 print("The first 10 rows are \n")
 for i in range(10):
    print (df.values[i],"\n")
 print("The dates are")
 print(df['Date'])
-print("Please enter Starting date \n")
-year = int(input('Enter a year'))
-month = int(input('Enter a month'))
-day = int(input('Enter a day'))
-date1 = datetime.date(year, month, day)
 
-#print("Please enter Ending date \n")
-#year = int(input('Enter a year'))
-#month = int(input('Enter a month'))
-#day = int(input('Enter a day'))
-#date2 = datetime.date(year, month, day)
-#date1=date1.timetuple()
-#print(df.values[0][0])
-date_string=input("Enter string date time as %Y-%m-%d %H:%M:%S")
+date_string=input("Enter starting date time(inclusive) as %Y-%m-%d %H:%M:%S")
 #date_string = "2020-07-15 00:00:00"
 
 date1 = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
 
-date_string=input("Enter string date time as %Y-%m-%d %H:%M:%S")
+date_string=input("Enter ending date time(exclusive) as %Y-%m-%d %H:%M:%S")
 #date_string = "2020-07-15 00:00:00"
 
 date2 = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
-
-
-#timestamp = datetime.datetime.timestamp(date)
-
-#print(date1,df.values[0][0])
-i=0;flag=0;
+dur=0;
+i=0;temp=i;no_of_days=0;
 while(date1!=df.values[i][0]):
-  i=i+1;
+  i=i+1;temp=i;
 while(date2!=df.values[i][0]):
   i=i+1;
-  if(flag==0):
-   if(df.values[i][3]=="studies" or df.values[i][3]=="Studies"):
-     dur=df.values[i][2]-df.values[i][1]
-  else:
-    if(df.values[i][3]=="studies" or df.values[i][3]=="Studies"):
-      dur=dur+df.values[i][2]-df.values[i][1]
-print("Total duration of studies during chosen period is ",dur);
+  if(df.values[i][3]=="studies" or df.values[i][3]=="Studies"):
+      t1=df.values[i][2];t2=df.values[i][1]
+      #print(t1," ",t2)
+      t1_mins=t1.hour*60+t1.minute
+      t2_mins=t2.hour*60+t2.minute
+      dur=dur+t1_mins-t2_mins
+  print(type(df.values[i-1][0]))
+  if(type(df.values[i-1][0])=='<class \'pandas._libs.tslibs.timestamps.Timestamp\'>'):
+    no_of_days=no_of_days+1;'<class \'pandas._libs.tslibs.timestamps.Timestamp'
+
+  #if(df.values[i-1][0]!= NaT):
+  #if(pd.isnull(np.datetime64(df.values[i-1][0])))
+
+study_hrs=math.floor(dur/60);study_mins=dur%60;
+print("Total duration of studies during chosen period is ",study_hrs, "hrs and ",study_mins," mins.")
+avg=dur/(no_of_days);
+avg_study_hrs=math.floor(avg/60);avg_study_mins=avg%60;
+print("Average time studied everyday during the choosen period is",avg_study_hrs, "hrs and ",avg_study_mins," mins.")
