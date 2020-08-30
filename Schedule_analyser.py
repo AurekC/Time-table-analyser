7721c8959e15b658d8bae039765748dd592fbf2e
aurek <aurekchattopadhyay@gmail.com>
2020-08-30 20:08:35 +0530

    Plotted graphs showing daily total time spent in studing for given duration for each category

--- Schedule_analyser.py
+++ Schedule_analyser.py
@@ -1,22 +1,32 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 import datetime
 import math
 
 
 def cal_avg(category,no_of_days):
+    x=[];y=[];sum=0;                        # sum calculates y value for plotting for every x
     dur=0;
     i=0;temp=i;
     while(date1!=df.values[i][0]):
       i=i+1;temp=i;
     while(date2!=df.values[i][0]):
       i=i+1;
-      if(df.values[i][3]==category):
+      if(pd.isnull(df.values[i-1][0])):             #pd.isnull returns true for null and nat
+        zz=0;#print("NaT")          #do nothing statement
+      else:
+          x.append(str(df.values[i-1][0]))
+          y.append(sum)
+          sum=0;
+      if(df.values[i][3].lower()==category.lower()):
           t1=df.values[i][2];t2=df.values[i][1]
           t1_mins=t1.hour*60+t1.minute
           t2_mins=t2.hour*60+t2.minute
           dur=dur+t1_mins-t2_mins
+          sum+=t1_mins-t2_mins
+
 
 
     dur_hrs=math.floor(dur/60);dur_mins=dur%60;
@@ -24,6 +34,10 @@ def cal_avg(category,no_of_days):
     avg=dur/(no_of_days);
     avg_dur_hrs=math.floor(avg/60);avg_dur_mins=avg%60;
     print("Average time spend everyday on ",category, " during the choosen period is",avg_dur_hrs, "hrs and ",avg_dur_mins," mins.")
+
+    plt.plot(x,y, label='linear')
+    plt.savefig(category+".pdf")
+    plt.show()
 #end of function
 
 f_log=open("log.txt","w")
@@ -44,7 +58,7 @@ date1 = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
 
 date_string=input("Enter ending date time(exclusive) as %Y-%m-%d")
 date_string+=" 00:00:00"
-#date_string = "2020-07-17 00:00:00"
+#date_string = "2020-07-20 00:00:00"
 date2 = datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
 
 i=0;no_of_days=0;
